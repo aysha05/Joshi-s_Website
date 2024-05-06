0c234736114d5e27e38044f8d16f63b51d6e199d
Aishwarya Kale <aishwaryakale05@gmail.com>
2024-05-06 12:09:22 +0300

    i am adding some function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var myFirstVariable;
 var element = document.getElementById('Hello');
 console.log(element);
+
+function scrollTop(){
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
