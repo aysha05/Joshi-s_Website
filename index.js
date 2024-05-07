708c6e56dc71f12dc29c26956f465e495081e89d
Sangam Watte <www.sangamwatteaaa@gmail.com>
2024-05-07 22:32:03 +0530

    Incorrect code removed.

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-var myFirstVariable;
-var element = document.getElementById('Hello');
-console.log(element);
+// var myFirstVariable;
+// var element = document.getElementById("Hello");
+// console.log(element);
 
-function scrollTop(){
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    })
+// function scrollTop() {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: "smooth",
+//   });
+// }
+
+// window.onscroll = function () {
+//   scrollFunction();
+// };
+
+// function scrollFunction() {
+//   var scrollToTopBtn = document.getElementById("scrollTop");
+//   if (document.body.scrollTop > 20) {
+//     scrollToTopBtn.style.display = "block";
+//   } else {
+//     scrollToTopBtn.style.display = "none";
+//   }
+// }
+
+function scroolToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function darkButtonClicked() {
+  var darkBtnElement = document.getElementById("dark-Btn");
+  console.log("Button Clicked");
+  alert("Button Clicked");
 }
